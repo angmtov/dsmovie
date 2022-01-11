c17ffb3723d1bf3cf4f62df7f0ea03099ac2d792
Angelica Tovar <angelicamtovar@gmail.com>
2022-01-11 00:48:13 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form(){
+
+    return (
+        <h1>Page Formulario</h1>
+    );
+
+    
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing(){
+
+    return (
+        <h1>Page Listagem</h1>
+    );
+
+    
+}
+export default Listing;
